4631fd1f1e2752aa586da0189fa8c86cf9f5a023
Tudor <tgoncear@gmail.com>
2021-10-21 16:11:38 +0200

    Com que ja tinc el contador funcionant i ting un metode reset implementant fare que el boto vaigui movent-se per la pantalla i el jugador vaigui a buscar-lo

--- app/src/main/java/me/tudorgoncear/dam/myapplication/MainActivity.kt
+++ app/src/main/java/me/tudorgoncear/dam/myapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package me.tudorgoncear.dam.myapplication
 
+import android.app.Activity
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.DisplayMetrics
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     //Inicialitzacio varables
@@ -16,12 +20,16 @@ class MainActivity : AppCompatActivity() {
     internal var temps = 60
     internal var started = false
     internal lateinit var timer : CountDownTimer
-    internal val countDown : Long = 60000
+    internal val countDown : Long = 10000
     internal val intervalCountDown : Long = 1000
+    val displayMetrics = DisplayMetrics();
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
         initCountDown();
         buttonTocam = findViewById(R.id.buttonTocam);
         nmTocam = findViewById(R.id.nmTocat);
@@ -40,6 +48,7 @@ class MainActivity : AppCompatActivity() {
     private fun startGame() {
         timer.start();
         started=true;
+        run();
     }
 
     private fun initCountDown() {
@@ -51,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 endGame();
+                resetGame();
             }
 
         }
@@ -63,5 +73,35 @@ class MainActivity : AppCompatActivity() {
     }
     private fun endGame(){
         Toast.makeText(this,getString(R.string.endGame), Toast.LENGTH_LONG).show();
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        with(alertDialogBuilder){
+            this.setTitle("PUNTUACIÓ");
+            this.setMessage("Has fet : " + counter + " clicks");
+            this.setPositiveButton("OK",null)
+            this.show();
+        }
+
+    }
+    private fun resetGame(){
+        counter = 0;
+        started = false;
+
+    }
+    private fun stawnRandomButton(){
+
+    }
+    private fun getWidthScreen(activity: Activity): Int{
+        activity.windowManager.defaultDisplay.getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+    private fun getHeightScreen(activity: Activity): Int{
+        activity.windowManager.defaultDisplay.getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+    private fun run(){
+        Random random = new Random();
+        buttonTocam.animate()
+            .x()
     }
 }
+
